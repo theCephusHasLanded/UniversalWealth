a79daae6c5b8b4d963222c82347c8d609421edf4
christinacephus@pursuit.org <christinacephus@pursuit.org>
2025-04-29 23:30:48 -0400

    Extremely simplify Vercel configuration to follow best practices

---
+++ vite.config.vercel.ts
@@ -0,0 +1,20 @@
+// Special version of vite.config.ts for Vercel deployment
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    sourcemap: false,
+  },
+  // No proxy setup for production
+});
